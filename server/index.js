c74bee9450076f380aa7ccaa234f9ace24455112
SamuelGustafsson <samuelgustafsson890623@gmail.com>
2018-01-17 17:14:12 +0100

    deployment fix

--- server/index.js
+++ server/index.js
@@ -33,13 +33,18 @@ app.use(bodyParser.json());
 require("./routes/authRoutes")(app);
 require("./routes/newsRoutes")(app);
 
-// Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
 
-// All remaining requests return the React app, so it can handle routing.
-app.get("*", function(request, response) {
-  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
 
 // Heroku skickar oss ett port nummer till process.env.PORT finns det ingen port så kör appen på port 5000 för localutveckling.
 const PORT = process.env.PORT || 5000;
